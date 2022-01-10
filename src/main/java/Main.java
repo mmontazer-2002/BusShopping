c4fb92260d52afcdb33a48b94b76a3c87f1bff9e
mmontazer-2002 <mmontazer1831@gmail.com>
2022-01-10 14:38:50 +0330

    add main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Mahdie Montazeralzohoor on 1/10/2022 2:37 PM
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
